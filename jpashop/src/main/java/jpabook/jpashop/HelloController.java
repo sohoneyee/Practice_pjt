306e600c6af827016403bf5fe7f69052637800cd
s_971001 <s_971001@naver.com>
2024-01-05 02:56:33 +0900

    240105 jpashop_진행중

---
+++ jpashop/src/main/java/jpabook/jpashop/HelloController.java
@@ -0,0 +1,15 @@
+package jpabook.jpashop;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("hello")
+    public String hello(Model model) {
+        model.addAttribute("data", "hello!!!");
+        return "hello"; // 이름이 hello인 화면(=hello.html)으로 리턴
+    }
+}
---
+++ jpashop/src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -0,0 +1,17 @@
+package jpabook.jpashop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpashopApplication {
+
+	public static void main(String[] args) {
+		hello hello = new hello();
+		hello.setData("hello");
+		String data = hello.getData();
+		System.out.println(data);
+		SpringApplication.run(JpashopApplication.class, args);
+	}
+
+}
